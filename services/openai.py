cb0a494968d2d45310bfe92f4e937d5ce6494623
Daniel Murrell <dsmurrell@gmail.com>
2023-04-26 22:16:52 +0100

    Cleaning

--- services/openai.py
+++ services/openai.py
@@ -45,51 +45,14 @@ def find_closest_sections(session: Session):
         model="text-embedding-ada-002",
     )
     query_embedding = res["data"][0]["embedding"]
-    print(query_embedding)
 
-    # Cast the target_embedding list to ARRAY(Float)
-    # target_embedding_array = cast(target_embedding, ARRAY(Float))
+    match_count = 10
 
-    # # Query the 10 sections with embeddings closest to the target embedding
-    # closest_sections = (
-    #     session.query(Section)
-    #     .filter(Section.embedding is not None)  # Ignore sections without an embedding
-    #     .order_by(func.pgv_distance(Section.embedding, target_embedding))
-    #     .limit(10)
-    #     .all()
-    # )
-
-    # match_threshold = 0.7  # Replace with your desired match threshold
-    # match_count = 10  # Replace with your desired match count
-
-    # # Define the similarity expression
-    # similarity_expr = 1 - func.cube_distance(Section.embedding, query_embedding)
-
-    # similarity_expr = 1 - func.vector_distance(Section.embedding, query_embedding)
-
-    # # Define the custom L2 distance operator
-    # def l2_distance(a, b):
-    #     return a.op("<->")(b)
-
-    # # Define the similarity expression
-    # similarity_expr = 1 - l2_distance(Section.embedding, query_embedding)
-
-    # Create the query using SQLAlchemy
-    # query = (
-    #     session.query(
-    #         Section.id,
-    #         Section.text,
-    #         similarity_expr.label("similarity"),
-    #     )
-    #     .filter(similarity_expr > match_threshold)
-    #     .order_by(similarity_expr.desc())
-    #     .limit(match_count)
-    # )
-
+    # Look up the match_count sections with an embedding closest to the query embedding
     query = session.scalars(
         select(Section)
         .order_by(Section.embedding.l2_distance(query_embedding))
-        .limit(10)
+        .limit(match_count)
     )
 
     # Execute the query and fetch the results
@@ -98,18 +61,6 @@ def find_closest_sections(session: Session):
     total_word_count = sum(len(section.text.split()) for section in closest_sections)
 
     print(f"Total word count: {total_word_count}")
-
-    # # Make sure to convert the list to a PgVector object
-    # target_vector = pgvector.Vector(target_embedding)
-
-    # # Query the 10 sections with embeddings closest to the target embedding
-    # closest_sections = (
-    #     session.query(Section)
-    #     .filter(Section.embedding is not None)  # Ignore sections without an embedding
-    #     .order_by(func.pgv_distance(Section.embedding, target_vector))
-    #     .limit(10)
-    #     .all()
-    # )
 
     # Print the results
     for section in closest_sections:
